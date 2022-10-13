7440e3e2a6fcb183847c94d96429d401340a7d2b
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-10-13 11:51:57 -0500

    Add Shared library Implement Webster apportionment

---
+++ Mowrer.FSWebster.Shared/Library.fs
@@ -0,0 +1,19 @@
+﻿namespace Mowrer.FSWebster.Shared
+
+module Webster =
+    let quotient population seats = float population / float (2 * seats + 1)
+    
+    let apportionSeats populations totalSeats =
+        let seatAllocations = Array.zeroCreate (populations |> Seq.length)
+        let quotients = Array.map2 quotient populations seatAllocations
+        
+        seq { 1..totalSeats } |> Seq.iter (fun _ ->
+            let maxQuotient = quotients |> Array.max
+            let maxIdx = quotients |> Array.findIndex (fun quotient -> quotient = maxQuotient)
+            
+            seatAllocations[maxIdx] <- seatAllocations[maxIdx] + 1
+            quotients[maxIdx] <- quotient populations[maxIdx] seatAllocations[maxIdx]
+            )
+        
+        seatAllocations
+        
