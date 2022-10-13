6dd316e8f3bfa47d158d1d2f3e61b704dac2e20e
ZTMowrer947 <25135050+ZTMowrer947@users.noreply.github.com>
2022-10-13 12:05:02 -0500

    Write out test seat distribution

--- Mowrer.FSWebster.CLI/Program.fs
+++ Mowrer.FSWebster.CLI/Program.fs
@@ -1,2 +1,14 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿namespace Mowrer.FSWebster.CLI
+
+open Mowrer.FSWebster.Shared
+
+module Main =
+    let names = [| "A"; "B"; "C"; "D" |]
+    let pops = [| 100000; 80000; 30000; 20000 |]
+    let seatDistribution = Webster.apportionSeats pops 8
+    
+    let entries = Array.map2 (fun pop name -> { Name = name; Population = pop }) pops names
+    
+    entries |> Array.iteri (fun idx entry -> printfn $"%s{entry.Name} with %d{entry.Population} folks gets %d{seatDistribution[idx]} seats")
+    
+    
